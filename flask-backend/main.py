9d97f020ccce0adf5b4415f6327813f4ac085262
Leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2024-05-27 17:14:19 +0300

    configure jwt and adding users to db with migrations

--- flask-backend/main.py
+++ flask-backend/main.py
@@ -1,13 +1,20 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_restx import Api, Resource, fields
 from config import DevConfig
-from models import Books
+from models import Books, User
 from exts import db
+from flask_migrate import Migrate
+from flask_cors import CORS
+from werkzeug.security import generate_password_hash
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
 app.config.from_object(DevConfig)
 
+CORS(app)
+
 db.init_app(app)
+migrate = Migrate(app,db)
 
 api=Api(app,doc='/docs')
 
@@ -23,37 +30,121 @@ books_model = api.model(
     }
 )
 
+signup_model = api.model(
+    "SignUp",
+    {
+        'id':fields.Integer(),
+        'first_name':fields.String(),
+        'last_name':fields.String(),
+        'email':fields.String(),
+        'password':fields.String()
+    }
+)
+
 @api.route('/hello')
 class HelloResource(Resource):
     def get(self):
         return {"message": "Hello World"}
 
-@api.route('/books')
+
+@api.route('/signup')
+class SignUp(Resource):
+    # @api.marshal_with(signup_model)
+    @api.expect(signup_model)
+    def post(self):
+        data=request.get_json()
+        print(f"Incoming data: {data}")
+        
+        if not data:
+            return {"message": "Invalid JSON"}, 400
+        
+        email=data.get('email')
+        
+        db_user=User.query.filter_by(email=email).first()
+        
+        if db_user is not None:
+            return {"message": "User account already exists"}, 409
+        
+        if db_user is not None:
+            return jsonify({"message":f"User account already exists"})
+        
+        try:
+            new_user=User(
+                first_name=data.get('first_name'),
+                last_name=data.get('last_name'),
+                email=data.get('email'),
+                password=generate_password_hash(data.get('password'))
+            )
+            
+            new_user.save()
+            return jsonify({"message":"User created successfully"})
+        
+        except Exception as e:
+            print(f"Error: {e}")
+            return {"message": "Internal Server Error"}, 500
+
+    
+@api.route('/login')
+class SignIn(Resource):
+    def post(self):
+        pass
+
+
+@api.route('/books', methods=['POST','GET'])
 class BooksResource(Resource):
 
     @api.marshal_list_with(books_model)
     def get(self):
         """Get all books from db"""
-        books=Books.querry.all()
+        books=Books.query.all()
         return books
     
-    def get(self):
+    @api.marshal_with(books_model)
+    @api.expect(books_model)
+    def post(self):
         """Create books"""
-        pass
+        data=request.get_json()
+        
+        new_book = Books(
+            title=data.get('title'),
+            author=data.get('author'),
+            description=data.get('description')
+        )
+        
+        new_book.save()
+        
+        return new_book, 201
     
 @api.route('/books/<int:id>')
 class BookResource(Resource):
+    
+    @api.marshal_list_with(books_model)
     def get(self, id):
         """Get a book by id"""
-        pass
-    
+        books=Books.query.get_or_404(id)
+        
+        return books
+        
+        
+    @api.marshal_with(books_model)
     def put(self, id):
         """Update books by id"""
-        pass
+        book_to_update=Books.query.get_or_404(id)
+        
+        data = request.get_json()
+        book_to_update.update(data.get("title"),data.get("author"),data.get("description"))
+        
+        return book_to_update
     
+
+    @api.marshal_with(books_model)
     def delete(self, id):
         """Delete books by id"""
-        pass
+        book_to_delete=Books.query.get_or_404(id)
+        
+        book_to_delete.delete()
+        
+        return book_to_delete
     
 
 @app.shell_context_processor
--- flask-backend/models.py
+++ flask-backend/models.py
@@ -29,3 +29,29 @@ class Books(db.Model):
     self.title=title
     self.author=author
     self.description=description
+    db.session.commit()
+
+
+# User model
+"""
+class User
+  id:int
+  firstname:string
+  last_name:string
+  email:string
+  password:string
+"""
+
+class User(db.Model):
+  id=db.Column(db.Integer(),primary_key=True)
+  first_name=db.Column(db.String(),nullable=False)
+  last_name=db.Column(db.String(),nullable=False)
+  email=db.Column(db.String(50),nullable=False)
+  password=db.Column(db.String(),nullable=False)
+  
+  def __repr__(self):
+    return f"<User {self.first_name}>"
+  
+  def save(self):
+    db.session.add(self)
+    db.session.commit()
