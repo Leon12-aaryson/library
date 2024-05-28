672d99ae6d80c3861d5dad1f99a9f366e3fc0cac
Leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2024-05-28 11:34:43 +0300

    close backend

--- src/components/books.jsx
+++ src/components/books.jsx
@@ -1,21 +1,10 @@
 import { response } from "express";
-import { useState,useEffect } from "react";
 
 export default function Books() {
-
-  useEffect(
-    ()=>{
-      fetch('book/hello')
-      .then(response=>response.json())
-      .then(data=>console.log(data))
-    },[]
-  )
-  const [message, setMessage] = useState('');
-
   return (
     <>
       <section>
-        <h1>{message}</h1>
+        <h1>Books</h1>
       </section>
     </>
   );
